a331d3875eea1849a862ca5140bc49792015b12f
권기덕 <merlin00@gwongideog-ui-iMac.local>
2017-08-03 23:59:12 +0900

    Emacs Configuration

---
+++ init.el
@@ -0,0 +1,12 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-cfg/emacs-cfg.org"))

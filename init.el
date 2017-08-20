9607e955a740bdef2feac49608e72324b0405451
권기덕 <merlin00@gwongideog-ui-iMac.local>
2017-08-20 22:23:24 +0900

    Updated

--- init.el
+++ init.el
@@ -10,3 +10,17 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-cfg/emacs-cfg.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet which-key use-package try solarized-theme pyvenv ox-reveal org-plus-contrib org-bullets magit jedi iedit highlight-indentation helm flycheck find-file-in-project exec-path-from-shell counsel company anaconda-mode ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

ee299888059c283a67b1493b15de5bba67c85f21
merlin00 <merlin00@iMac.local>
2017-11-22 15:56:43 +0900

    Reset

--- init.el
+++ init.el
@@ -4,8 +4,6 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-	     '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -19,8 +17,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
+<<<<<<< HEAD
+	 (quote
+		(ob-ipython ein jedi elpy yasnippet flycheck exec-path-from-shell org-plus-contrib which-key use-package try swiper-helm ox-reveal org-bullets magit iedit color-theme-solarized ace-window ac-ispell))))
+=======
    (quote
-    (elpy htmlize yasnippet which-key use-package try solarized-theme pyvenv ox-reveal org-plus-contrib org-bullets magit jedi iedit highlight-indentation helm flycheck find-file-in-project exec-path-from-shell counsel company ace-window))))
+    (elpy htmlize yasnippet which-key use-package try solarized-theme pyvenv ox-reveal org-plus-contrib org-bullets magit jedi iedit highlight-indentation helm flycheck find-file-in-project exec-path-from-shell counsel company anaconda-mode ace-window))))
+>>>>>>> parent of 9604cd3... deleted anaconda mode.
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

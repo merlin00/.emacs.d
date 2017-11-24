c86786ffc77584f3e3fee93bce0d57b34f72e889
merlin00 <merlin00@iMac.local>
2017-11-24 19:43:51 +0900

    changed config.

--- init.el
+++ init.el
@@ -10,5 +10,19 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/org-cfg/emacs-cfg.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (htmlize which-key use-package try swiper-helm ox-reveal org-plus-contrib org-bullets ob-ipython magit jedi iedit flycheck exec-path-from-shell elpy ein color-theme-solarized anaconda-mode ace-window ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

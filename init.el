e229c43a30ce66a5e6ddf95cdae1c66773c7f37e
권기덕 <merlin00@iMac.local>
2018-04-13 20:16:26 +0900

    Changed config.org.

--- init.el
+++ init.el
@@ -5,24 +5,9 @@
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-
+(package-refresh-contents)
+ 
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (htmlize which-key use-package try swiper-helm ox-reveal org-plus-contrib org-bullets ob-ipython magit jedi iedit flycheck exec-path-from-shell elpy ein color-theme-solarized anaconda-mode ace-window ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
